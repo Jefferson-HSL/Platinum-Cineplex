26bada510078c2ba28e3decfedcb50950ba82946
Jefferson-HSL <santoslima963@gmail.com>
2022-05-20 15:54:04 -0300

    added a modal in the first recommended movie

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const modal = document.querySelector('.modal-container');
 
 function openModal() {
   modal.classList.add('active');
+
+  const body = document.querySelector('body');
+  body.classList.add('no-scroll');
 }
 
 function closeModal() {
